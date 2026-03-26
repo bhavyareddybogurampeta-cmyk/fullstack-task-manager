4233d9ede080baeed44e032bc2c8512990d9e231
bhavyareddybogurampeta-cmyk <bhavyareddybogurampeta@gmail.com>
2026-03-26 10:32:32 -0400

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Full Stack Task Manager</h1>
+      <p>Starter React frontend for task management.</p>
+    </div>
+  );
+}
+
+export default App;
